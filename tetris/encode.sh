9d88bff8d5a697f59dcd6aba1d0f51b9843b6fec
James Denholm <jamesdenholm15@gmail.com>
2017-05-26 13:58:37 +1000

    The Arcade

--- tetris/encode.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/sh
-dir=src/sounds
-oggenc -q 0 -o $dir/$1.ogg $dir/$1.wav
-lame -b 64 $dir/$1.wav $dir/$1.mp3
--- tetris/src/scripts/main.js
+++
@@ -1,3 +0,0 @@
-require(['config', 'tetris/UserInterface'], function (_, UserInterface) {
-  new UserInterface();
-});
